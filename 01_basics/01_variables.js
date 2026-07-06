42e3b8d4a4fb1b64da0f5fd2e76698c245dcb3f7
Krish Sukhariya <krishsukhariya@gmail.com>
2026-07-06 11:29:26 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+// const accountId = 2450690
+// let   accountEmail = "ad@abgmail.com"
+// var  accountPassword = 12345
+// accountCity = Delhi
+// let State;
+
+// console.table([accountId,accountEmail,accountPassword,accounCity,accountState])
+const accountId = 144553
+let accountEmail = "Abdc@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "krish@email.com"
+accountPassword = "21212121"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
